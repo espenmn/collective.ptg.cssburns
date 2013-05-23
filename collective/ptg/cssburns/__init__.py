8ea58b2f3e3b88593348db5766d38927ad154dbf
Espen Moe-Nilssen <espen@medialog.no>
2013-05-23 21:32:13 +0200

    Changing to "base display type" instead of "batching"

--- collective/ptg/cssburns/__init__.py
+++ collective/ptg/cssburns/__init__.py
@@ -1,7 +1,6 @@
 from collective.plonetruegallery.utils import createSettingsFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from collective.plonetruegallery.browser.views.display import \
-    BatchingDisplayType
+from collective.plonetruegallery.browser.views.display import BaseDisplayType
 from collective.plonetruegallery.interfaces import IBaseSettings
 from zope import schema
 from zope.i18nmessageid import MessageFactory
@@ -73,7 +72,7 @@ class ICssburnsDisplaySettings(IBaseSettings):
             default=u"Padding below imagedescription"),
         default=u"70px")
 
-class CssburnsDisplayType(BatchingDisplayType):
+class CssburnsDisplayType((BaseDisplayType):
     name = u"cssburns"
     schema = ICssburnsDisplaySettings
     description = _(u"label_cssburns_display_type",
@@ -85,6 +84,9 @@ class CssburnsDisplayType(BatchingDisplayType):
     def css(self):
         return u"""
 <style>
+#CSS3Slideshow .img3{
+border: %(batch)ipx solid black;
+}
 </style>
 <link rel="stylesheet" type="text/css" href="++resource++ptg.cssburns/style.css"/>
 """ % {
@@ -93,6 +95,7 @@ class CssburnsDisplayType(BatchingDisplayType):
         'opacity': self.settings.cssburns_opacity,
         'bottompadding' : self.settings.cssburns_bottompadding,
         'toppadding' : self.settings.cssburns_toppadding,
+        'number_of_images' : self.adapter.cooked_images,
        }
 
 CssburnsSettings = createSettingsFactory(CssburnsDisplayType.schema)
